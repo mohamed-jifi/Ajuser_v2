d524b2541ee8b105cb0ac8669e8f378e0b7fdf99
MOHAMED JIFI <91312743+mohamed-jifi@users.noreply.github.com>
2021-09-27 13:02:21 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,29 +11,29 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://ibb.co/M5CXDQh";
+  r_text[0] = "https://i.ibb.co/cK5fFMN/logo.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*TOXIC MWON*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*Ajuser_v2*
  
 
-        *Github link        _https://github.com/aju001/Ajuser_v2_*
+        *Github link        _https://github.com/mohamed-jifi/Toxic-Mwon* 🦨
         
         
-       *Bot making video   _https://youtu.be/dm_kVZ0m2eY_*
+       *Instagram         _https://www.instagram.com/mr.covid_.19_* 📺
  
 
-       *Audio commads-1     _https://github.com/aju001/Ajuser_v2/tree/master/upload_*
+       *Audio commads-1     _https://github.com/mohamed-jifi/Toxic-Mwon/tree/master/upload_* 🎧
  
  
-       *Audio commads-2     _https://github.com/aju001/Ajuser_v2/tree/master/uploads_*
+       *Audio commads-2     _https://github.com/mohamed-jifi/Toxic-Mwon/tree/master/uploads_* 🎧🎶
   
   
-      *Sticker commads      _https://github.com/aju001/Ajuser_v2/tree/master/stickers_*
+      *Sticker commads      _https://github.com/mohamed-jifi/Toxic-Mwon/tree/master/stickers_* 🦋🎋
 `}) 
 
 }));
